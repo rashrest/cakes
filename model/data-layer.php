25c4e515511cc661203457408d34c7a426a68183
David Boone <Dboone2@mail.greenriver.edu>
2021-05-13 13:25:36 -0400

    added data-layer.php

---
+++ model/data-layer.php
@@ -0,0 +1,9 @@
+<?php
+
+//data-layer.php
+
+function getFlavors()
+{
+    return array("grasshopper"=>"The Grasshopper","maple"=>"Whiskey Maple Bacon","carrot"=>"Carrot Walnut",
+        "caramel"=>"Salted Caramel Cupcake","velvet"=>"Red Velvet","lemon"=>"Lemon Drop","tiramisu"=>"Tiramisu");
+}
